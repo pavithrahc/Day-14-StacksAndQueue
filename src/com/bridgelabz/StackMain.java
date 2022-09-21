cf4f54a18d6493cfaa93a52fd9f94a0c76c7a3da
pavithramuddu93 <pavithramuddu93>
2022-09-21 21:34:27 +0530

    Welcome

---
+++ src/com/bridgelabz/StackMain.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.bridgelabz;
+
+/**
+ * @author PAVITHRA C
+ *
+ */
+public class StackMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Stack And Queue");
+	}
+
+}
